a3b0dca32a3b5e20f8918effb0f1418073808c2d
Matt <nevers.matthew@gmail.com>
2023-10-19 10:21:23 -0400

    each

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,22 @@
 resource "aws_instance" "server"{
+    for_each = {
+        prod = aws_subnet.subnet-prd-a.id
+        dev = aws_subnet.subnet-dev-a.id
+    }
+
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
-    count = 2
-    subnet_id = aws_subnet.subnet-dev-a.id    
+    subnet_id = each.value
 
     tags = {
-        Name = "Dev ${count.index}"
+        Name = each.key
     }
 }
 
 output "private-ips"{
-    value = aws_instance.server[*].private_ip
+    value = [ for instance in aws_instance.server : instance.private_ip] 
 }
 
 output "dev-names"{
-    value = aws_instance.server[*].tags.Name
+    value = [ for instance in aws_instance.server : instance.tags.Name] 
 }
