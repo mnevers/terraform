7b9ba52282b56d6ddedf3801e9f0388225a753ec
Matt <nevers.matthew@gmail.com>
2023-10-21 13:04:49 -0400

    dynamic block

--- main.tf
+++ main.tf
@@ -7,6 +7,28 @@ provider "aws"{
     region = "us-west-2"
 }
 
+locals {
+    ingress_rules = [{
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+    },
+    {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+    },
+    {        
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+    }]
+}
+
+locals {
+    my_ip = ["73.69.109.181/32"]
+}
+
 #Main not so used. check out Network.tf and others
 
 resource "aws_security_group" "allow-ssh-web"{
@@ -14,30 +36,18 @@ resource "aws_security_group" "allow-ssh-web"{
     description = "Allow inbound traffic"
     vpc_id = aws_vpc.vpc-prd.id
 
-    ingress {
-        description = "HTTPS"
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["73.69.109.181/32"]
-    }
+    dynamic "ingress" {
+        for_each = local.ingress_rules
 
-    ingress {
-        description = "HTTP"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["73.69.109.181/32"]
+        content {
+            description = ingress.value.description
+            from_port = ingress.value.from_port
+            to_port = ingress.value.to_port
+            protocol = "tcp"
+            cidr_blocks = local.my_ip
+        }
     }
-
-    ingress {
-        description = "SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["73.69.109.181/32"]
-    }
-
+   
     egress {
         from_port = 0
         to_port = 0
