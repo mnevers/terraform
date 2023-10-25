b37d3f6fabf2e02e1243f4f40f2054787737e486
Matt <nevers.matthew@gmail.com>
2023-10-24 20:26:15 -0400

    var naming convention

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "default-vpc-out"{
+output "default_vpc_out"{
     value = var.boolean == true ? data.aws_vpc.default-vpc.cidr_block : "Sensored Message"
     description = "Print out default vpc or if a boolean of yes in a var was given sensor message (if block test)"
 }
 
-output "ansible_prv_ip-out" {
+output "ansible_prv_ip" {
     value = aws_instance.ansible_managed.private_ip
 }
 
-output "ansible_pub_ip-out" {
+output "ansible_pub_ip" {
     value = aws_instance.ansible_managed.public_ip
 }
 
